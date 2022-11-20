e3d1d1e7714a7b7fbba9ff1030433f806cd9e45d
Raul Vergara Ortiz <raul.vergara@pragma.com.co>
2022-11-19 21:41:53 -0500

    add axios

--- apigateway/code-lambdas/index.js
+++ apigateway/code-lambdas/index.js
@@ -1,13 +1,40 @@
+var axios = require('axios');
+
 exports.handler = async (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify([{ "data": "hola" }]),
+  try {
+    const list_pokemon = await getListPokemons(1, 0)
+    return createResponse(200, list_pokemon);
+  }
+  catch (ex) {
+    return createResponse(500, null);
+  }
+};
+
+function createResponse(statusCode, body) {
+  return response = {
+    statusCode: statusCode,
+    body: JSON.stringify(body),
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': 'true'
     }
   };
+}
 
-  return response;
-};
+async function getListPokemons(limit, page) {
+
+  var config = {
+    method: 'get',
+    url: `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${page}`,
+    headers: {}
+  };
+
+  return await axios(config)
+    .then(function (response) {
+      return response.data
+    })
+    .catch(function (error) {
+      throw new Error('Se presentó un error');
+    });
+}
